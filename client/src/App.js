b168076a258e3b0b8f68f94188f3596268e41020
SLeBron <sophieclebron@gmail.com>
2023-04-28 18:14:07 -0600

    added react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Routes>
+          
+        </Routes>
+      </Router>
     </div>
   );
 }
