4c1d4686bb9cae2b694ff31cef009da60831b457
SLeBron <sophieclebron@gmail.com>
2023-04-30 23:30:11 -0600

    updated app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Homepage from './pages/homepage/homepage.js';
+import Park from './pages/park/park.js';
+import Profile from './pages/profile/profile.js';
 import './App.css';
 
 function App() {
@@ -7,7 +10,9 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-          
+          <Route path='/national-parks-bucket-list/' element={<Homepage/>}/>
+          <Route path='/Park' element={<Park/>}/>
+          <Route path='/Profile' element={<Profile/>}/>
         </Routes>
       </Router>
     </div>
