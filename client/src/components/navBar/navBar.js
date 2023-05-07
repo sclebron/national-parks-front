49651373e28d4290546f2d272bfbeaec5aa6b4ff
SLeBron <sophieclebron@gmail.com>
2023-05-07 17:43:11 -0600

    added dropdown items

--- client/src/components/navBar/navBar.js
+++ client/src/components/navBar/navBar.js
@@ -6,7 +6,17 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 function navBar() {
     return (
         <Navbar bg="light" expand="lg" className="navbar">
-
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                    Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                    Separated link
+                </NavDropdown.Item>
+            </NavDropdown>
         </Navbar>
     )
 }
