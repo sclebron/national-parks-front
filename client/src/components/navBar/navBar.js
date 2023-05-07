be3ccd0d820588add2bb136c49f9400d490d0f74
SLeBron <sophieclebron@gmail.com>
2023-05-07 17:34:54 -0600

    started navbar

---
+++ client/src/components/navBar/navBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './navBar.css';
+import { Link } from "react-router-dom";
+
+function navBar() {
+    return (
+        <div className="navBar">
+            
+        </div>
+    )
+}
+
+export default navBar;
