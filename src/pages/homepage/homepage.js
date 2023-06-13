647a4bd195d7359daf56a1eff8736448fb9daf6a
SLeBron <sophieclebron@gmail.com>
2023-06-13 17:34:27 -0600

    started homepage.js

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './homepage.css';
+
+function Homepage () {
+    return (
+        <div className="home">
+            
+        </div>
+    )
+}
+
+export default Homepage;
