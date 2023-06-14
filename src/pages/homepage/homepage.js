59cf86fb76cad19c2b0bc0dcd056d95e9df3574c
SLeBron <sophieclebron@gmail.com>
2023-06-14 14:00:44 -0600

    added background image to homepage

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import navBar from './navBar.js';
 import './homepage.css';
 
 function Homepage () {
     return (
-        <div className="home">
-            
+        <div className="homeContainer">
+            <navBar />
+            <div className="home">
+                <div style={{ backgroundImage:  }} className="backgroundImg">
+                Welcome To Your National Parks Bucket List!
+                </div>
+            </div>
         </div>
     )
 }
