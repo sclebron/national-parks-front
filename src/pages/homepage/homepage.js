07553f3d1cf6ae781ab44ac5f7d851bc5ab51de8
SLeBron <sophieclebron@gmail.com>
2023-06-14 14:07:28 -0600

    started function to make background images slideshow

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -1,8 +1,40 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
 import navBar from './navBar.js';
 import './homepage.css';
 
+
+
 function Homepage () {
+    const [image, setImage] = useState(1);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            change();
+        }, 10000);
+
+        return () => {
+            clearInterval(interval);
+        };
+    }, [photo]);
+
+    const change = () => {
+        if (image === 5) {
+            setImage(1);
+            return;
+        }
+
+        setPhoto((prev) => prev + 1);
+
+        const returnImageUrl = () => {
+            switch(image) {
+                case 1: 
+                    return './images/img1.jpg';
+                
+            }
+        }
+    }
+
     return (
         <div className="homeContainer">
             <navBar />
