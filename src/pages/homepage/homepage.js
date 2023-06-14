63a2119a53a8cbb5f1c1c4a583db05029f997436
SLeBron <sophieclebron@gmail.com>
2023-06-14 14:11:16 -0600

    updated images for slideshow

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -25,21 +25,28 @@ function Homepage () {
         }
 
         setPhoto((prev) => prev + 1);
+    }
 
         const returnImageUrl = () => {
             switch(image) {
                 case 1: 
                     return './images/img1.jpg';
-                
+                case 2: 
+                    return './images/img2.jpg';
+                case 3: 
+                    return './images/img3.jpg';
+                case 4: 
+                    return './images/img4.jpg';
+                case 5: 
+                    return './images/img5.jp';
             }
         }
-    }
 
     return (
         <div className="homeContainer">
             <navBar />
             <div className="home">
-                <div style={{ backgroundImage:  }} className="backgroundImg">
+                <div style={{ backgroundImage: `url(${returnImageUrl()})` }} className="backgroundImg">
                 Welcome To Your National Parks Bucket List!
                 </div>
             </div>
