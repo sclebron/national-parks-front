5dfc2c03946cb9d833806d827420c983e8c89e2c
SLeBron <sophieclebron@gmail.com>
2023-06-16 21:41:00 -0600

    imported navbar into header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import Dropdown from 'react-bootstrap/Dropdown';
-import DropdownButton from 'react-bootstrap/DropdownButton';
+import NavBar from './components/NavBar/NavBar.js';
 import './header.css';
 
 function Header() {
     return (
         <div className="header">
-            <DropdownButton id="dropdown-item-button" title="Dropdown button">
-                <Dropdown.ItemText>National Parks</Dropdown.ItemText>
-                <Dropdown.Item as="button">Action</Dropdown.Item>
-                <Dropdown.Item as="button">Another action</Dropdown.Item>
-                <Dropdown.Item as="button">Something else</Dropdown.Item>
-            </DropdownButton>
+            <NavBar />
         </div>
     )
 }
