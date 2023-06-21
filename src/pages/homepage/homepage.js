bb96c931d5b1221821d2afa6bbe2cb6e6b7048f4
SLeBron <sophieclebron@gmail.com>
2023-06-20 18:25:32 -0600

    updated background image on homepage

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -1,52 +1,17 @@
 import React from 'react';
 // import { useEffect, useState } from 'react';
-import gtnp from './images/gtnp.jpg';
+import npImg from './images/backgroundImageNP.jpg';
 import Header from '/Users/sophielebron/Desktop/coding-projects/national-parks-front/src/components/header/header.js';
 // import Header from '/components/header/header.js';
 import './homepage.css';
 
 function Homepage () {
-    // const [image, setImage] = useState(1);
-
-    // useEffect(() => {
-    //     const interval = setInterval(() => {
-    //         change();
-    //     }, 10000);
-
-    //     return () => {
-    //         clearInterval(interval);
-    //     };
-    // }, [image]);
-
-    // const change = () => {
-    //     if (image === 5) {
-    //         setImage(1);
-    //         return;
-    //     }
-
-    //     setImage((prev) => prev + 1);
-    // }
-
-    //     const returnImageUrl = () => {
-    //         switch(image) {
-    //             case 1: 
-    //                 return './images/rmnp.jpg';
-    //             case 2: 
-    //                 return './images/gtnp.jpg';
-    //             case 3: 
-    //                 return './images/ynp.jpg';
-    //             case 4: 
-    //                 return './images/gsdnp.jpg';
-    //             case 5: 
-    //                 return './images/bcgnp.jp';
-    //         }
-    //     }
 
     return (
         <div className="homeContainer">
             <Header />
             <div className="home">
-                <div style={{ backgroundImage: `url(${gtnp})` }} className="backgroundImg">
+                <div style={{ backgroundImage: `url(${npImg})` }} className="backgroundImg">
                 Welcome To Your National Parks Bucket List!
                 </div>
             </div>
