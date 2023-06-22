3fa844a8a7d48cb2637298714ce67455d8187345
SLeBron <sophieclebron@gmail.com>
2023-06-21 22:16:34 -0600

    updating

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -1,9 +1,7 @@
 import React from 'react';
-// import { useEffect, useState } from 'react';
 import npImg from './images/backgroundImgNP.jpg';
 // import Header from '/Users/sophielebron/Desktop/coding-projects/national-parks-front/src/components/header/header.js';
 //instead of importing header, import individual National Parks, Login, and Sign Up buttons
-// import Header from '/components/header/header.js';
 import './homepage.css';
 
 function Homepage () {
