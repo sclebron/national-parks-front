943c1bbda2f529f761fb4dce744cbe5f37bb5d50
SLeBron <sophieclebron@gmail.com>
2023-06-22 20:37:47 -0600

    moved navbar from app.js to navbar file

---
+++ src/components/navBar/navBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './navBar.css';
+
+function NavBar() {
+    return (
+    <div className="menu-container">
+        <div classname="menu-trigger">National Parks</div>
+        <div className="dropdown-menu"></div>
+    </div>
+    )
+}
+
+export default NavBar;
