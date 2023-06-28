1953d3992312d013f8eacfebc931bf601141a1c9
SLeBron <sophieclebron@gmail.com>
2023-06-28 16:05:53 -0600

    updated navbar

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -5,7 +5,12 @@ function NavBar() {
     return (
     <div className="menu-container">
         <div classname="menu-trigger">National Parks</div>
-        <div className="dropdown-menu"></div>
+        <div className="dropdown-menu">
+            <h3>The Kiet<br/><span>>Website Designer</span></h3>
+            <ul>
+                <DropdownItem/>
+            </ul>
+        </div>
     </div>
     )
 }
