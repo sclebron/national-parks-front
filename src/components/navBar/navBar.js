7f932d1db4538055ec7a8fe800fd77c637c8c616
SLeBron <sophieclebron@gmail.com>
2023-06-30 13:24:00 -0600

    used state for opening navbar menu

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import { UNSAFE_mapRouteProperties } from 'react-router';
+import React, { useState } from 'react';
+// import { UNSAFE_mapRouteProperties } from 'react-router';
 import './navBar.css';
 
 function NavBar() {
+
+    const [open, setOpen ] = useState(false);
+
     return (
     <div className="menu-container">
-        <div classname="menu-trigger">National Parks</div>
-        <div className="dropdown-menu">
+        <div classname="menu-trigger" onClick={()=>{setOpen(!open)}}>National Parks</div>
+        <div className={`dropdown-menu ${open? 'active' : 'inactive'}`}>
             <h3>The Kiet<br/><span>Website Designer</span></h3>
             <ul>
                 <DropdownItem img = {user} text = {"My Profile"}/>
