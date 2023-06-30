89967e02dd8ee778cb8d15c807670df4daa6d711
SLeBron <sophieclebron@gmail.com>
2023-06-30 14:19:37 -0600

    added useEffect and useRef so clicking outside navbar closes it

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -1,14 +1,28 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './navBar.css';
 
 function NavBar() {
 
     const [open, setOpen ] = useState(false);
 
-    useEffect()
+    let menuRef = useRef();
+
+    useEffect(() => {
+        let handler = (e) => {
+            if(!menuRef.current.contains(e.target)) {
+                setOpen(false);
+            }
+        };
+
+        document.addEventListener("mousedown", handler)
+
+        return() => {
+            document.removeEventListener("mousedown", handler);
+        }
+    });
 
     return (
-    <div className="menu-container">
+    <div className="menu-container" ref={menuRef}>
         <div classname="menu-trigger" onClick={()=>{setOpen(!open)}}>National Parks</div>
         <div className={`dropdown-menu ${open? 'active' : 'inactive'}`}>
             <ul>
