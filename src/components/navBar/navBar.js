3726854e96b04e4d76af3fce0c341e7ae0ba3581
SLeBron <sophieclebron@gmail.com>
2023-07-06 15:46:29 -0600

    updating

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import './navBar.css';
 
 function NavBar() {
