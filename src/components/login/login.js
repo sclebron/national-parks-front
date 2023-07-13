9d150eb811e9643784b0f89b4590665097b441d5
SLeBron <sophieclebron@gmail.com>
2023-07-13 17:50:38 -0600

    implemented useState for showing login modal

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useState }from 'react';
 import './login.css';
 
 function Login() {
-    
+
+    const loginModal = () => {
+        const [isShowing, setIsShowing] = useState(false)
+
+        function toggle() {
+            setIsShowing(!isShowing)
+        }
+    }
 
     return (
         <div className="login">
