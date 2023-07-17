b2b2b8f2ac254d1ca037f0f1f3cd258d98008903
SLeBron <sophieclebron@gmail.com>
2023-07-17 14:25:48 -0600

    updated login modal popup

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -4,11 +4,16 @@ import { MdClose } from 'react-icons/md';
 
 function Login() {
 
-    
+    const [showPopup, setShowPopup] = useState(false)
 
     return (
         <div className="login">
-            <button className="loginBtn">Login</button>
+            <div className="modalBtn">
+                <button className="loginBtn" onClick={() => setShowPopup(!showPopup)}>Login</button>
+            </div>
+            <div className="loginModal">
+                
+            </div>
         </div>
     )
 }
