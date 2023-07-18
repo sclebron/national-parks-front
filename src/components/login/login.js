bfbaec8fa5ae6ec840704e4f8bb1e88a476b94e5
SLeBron <sophieclebron@gmail.com>
2023-07-18 15:00:39 -0600

    updated login btn styling

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -4,25 +4,15 @@ import { MdClose } from 'react-icons/md';
 
 function Login() {
 
-    const [showPopup, setShowPopup] = useState(false);
 
-    const toggleMOdal = () => {
-        setModal(!modal);
-    };
-
-    if(modal) {
-        document.body.classList.add('active-modal')
-    } else {
-        document.body.classList.remove('active-modal');
-    }
 
     return (
         <div className="login">
-            <button className="loginBtn" onClick={toggleModal}>Login</button>
-            <div className="loginModal" style={{visbility: showPopup ? 'visibile' : 'hidden'}}>
+            <button className="loginBtn">Login</button>
+            <div className="loginModal">
                 <div className="modalBorder"></div>
                 <div className="modalForm">
-                    <MdClose size={25} className="closeIcon" onClick={toggleModal} />
+                    <MdClose size={25} className="closeIcon" />
                     <div className="modalContent">
                         <div className="email"></div>
                         <div className="password"></div>
