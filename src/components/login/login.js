c8c96d245cdb921db9481b77913f66de1f26ad30
SLeBron <sophieclebron@gmail.com>
2023-07-18 14:16:50 -0600

    tried new method for login popup modal

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -4,15 +4,25 @@ import { MdClose } from 'react-icons/md';
 
 function Login() {
 
-    const [showPopup, setShowPopup] = useState(false)
+    const [showPopup, setShowPopup] = useState(false);
+
+    const toggleMOdal = () => {
+        setModal(!modal);
+    };
+
+    if(modal) {
+        document.body.classList.add('active-modal')
+    } else {
+        document.body.classList.remove('active-modal');
+    }
 
     return (
         <div className="login">
-            <button className="loginBtn" onClick={() => setShowPopup(!showPopup)}>Login</button>
+            <button className="loginBtn" onClick={toggleModal}>Login</button>
             <div className="loginModal" style={{visbility: showPopup ? 'visibile' : 'hidden'}}>
                 <div className="modalBorder"></div>
                 <div className="modalForm">
-                    <MdClose size={25} className="closeIcon" onClick={() => setShowPopup(false)} />
+                    <MdClose size={25} className="closeIcon" onClick={toggleModal} />
                     <div className="modalContent">
                         <div className="email"></div>
                         <div className="password"></div>
