1da48dc7366950dcfd80897107df6fb9a1eab16e
SLeBron <sophieclebron@gmail.com>
2023-07-19 15:32:39 -0600

    added button to close login modal

--- src/components/login/loginModal.js
+++ src/components/login/loginModal.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
-function Modal({ open, children }) {
+function Modal({ open, children, onClose }) {
     if (!open) return null
-    
+
     return (
         <div>
+            <button onClick={onClose}>Close Modal</button>
             {children}
         </div>
     )
