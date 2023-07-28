6bb3fe8e6371ee0b6e76f6e3cfdf70d170291b58
SLeBron <sophieclebron@gmail.com>
2023-07-28 17:17:31 -0600

    added sign up modal

--- src/components/signUp/signUp.js
+++ src/components/signUp/signUp.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './signUp.css';
+import Modal from './signUpModal.js';
 
 function SignUp() {
+
+    const [isOpen, setIsOpen] = useState(false)
+
     return (
         <div className="signup">
-            <button className="signupBtn">Sign Up</button>
+            <button className="signupBtn" onClick={() => setIsOpen(true)}>Sign Up</button>
+            <Modal className="signupModal" open={isOpen} onClose={() => setIsOpen(false)}>
+                <br></br>
+                <div className="signupModalHeader">Create Account</div>
+                <br></br>
+                <div>Username:</div>
+                <input></input>
+                <br></br>
+                <div>Password:</div>
+                <input></input>
+                <br></br>
+                <div>Confirm Password:</div>
+                <input></input>
+                <br></br>
+                <button className="signupModalBtn">Sign Up</button>
+            </Modal>
         </div>
     )
 }
---
+++ src/components/signUp/signUpModal.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { MdClose } from 'react-icons/md';
+import './signUpModal.css';
+
+function Modal({ open, children, onClose }) {
+    if (!open) return null
+
+    return (
+        <div className="modal">
+            <MdClose className="closeBtn" onClick={onClose}>Close Modal</MdClose>
+            {children}
+        </div>
+    )
+}
+
+export default Modal;
