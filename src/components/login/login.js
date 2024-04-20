250a2e701348bae2f85566cf2dfd4b1a0182c889
SLeBron <sophieclebron@gmail.com>
2024-04-19 19:30:31 -0600

    started redesigning login modal

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -7,19 +7,31 @@ function Login() {
     const [isOpen, setIsOpen] = useState(false)
 
     return (
+        // <div className="login">
+        //     <button className="loginBtn" onClick={() => setIsOpen(true)}>Login</button>
+        //     <Modal className="loginModal" open={isOpen} onClose={() => setIsOpen(false)}>
+        //         <br></br>
+        //         <div className="loginModalHeader">Login</div>
+        //         <br></br>
+        //         <input placeholder="Username" className="userInput"></input>
+        //         <br></br>
+        //         <input placeholder="Password" className="passwordInput"></input>
+        //         <br></br>
+        //         <button className="loginModalBtn">Login</button>
+        //     </Modal>
+        // </div>
         <div className="login">
-            <button className="loginBtn" onClick={() => setIsOpen(true)}>Login</button>
-            <Modal className="loginModal" open={isOpen} onClose={() => setIsOpen(false)}>
-                <br></br>
-                <div className="loginModalHeader">Login</div>
-                <br></br>
-                <input placeholder="Username" className="userInput"></input>
-                <br></br>
-                <input placeholder="Password" className="passwordInput"></input>
-                <br></br>
-                <button className="loginModalBtn">Login</button>
-            </Modal>
-        </div>
+                <Modal className="loginModal" open={isOpen} onClose={() => setIsOpen(false)}>
+                    <br></br>
+                    <div className="loginModalHeader">Login</div>
+                    <br></br>
+                    <input placeholder="Username" className="userInput"></input>
+                    <br></br>
+                    <input placeholder="Password" className="passwordInput"></input>
+                    <br></br>
+                    <button className="loginModalBtn">Login</button>
+                </Modal>
+            </div>
     )
 }
 
