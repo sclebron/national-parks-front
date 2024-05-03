81cb45076e01680663ffaf359069fbea3e9d2230
SLeBron <sophieclebron@gmail.com>
2024-05-03 16:41:06 -0600

    added images

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -1,6 +1,5 @@
 import React from 'react';
-// import Modal from './login/loginModal.js';
-import Modal from '/Users/sophielebron/Desktop/coding-projects/national-parks-front/src/components/login/loginModal.js';
+import peanutTeton from './images/peanutTeton.png';
 import './homepage.css';
 
 function Homepage () {
@@ -11,8 +10,7 @@ function Homepage () {
                 <div>
                     <h1 className="hometext">Welcome To Your National Parks Bucket List</h1>
                     <div classname="home-menu">Let The Adventure Begin!</div>
-                    {/* <Modal className="login-modal"></Modal> */}
-                    <div className="pic-1"></div>
+                    <div style={{ backgroundImage: `url(${peanutTeton})` }}className="pic-1"></div>
                     <div className="pic-2"></div>
                     <div className="pic-3"></div>
                     <div className="pic-4"></div>
