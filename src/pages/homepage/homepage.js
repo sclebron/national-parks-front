d3783cb7a091fec627ef202dbbf8c18441e1df97
SLeBron <sophieclebron@gmail.com>
2024-05-03 16:54:28 -0600

    added more images and reinstalled node modules

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -11,11 +11,11 @@ function Homepage () {
                     <h1 className="hometext">Welcome To Your National Parks Bucket List</h1>
                     <div classname="home-menu">Let The Adventure Begin!</div>
                     <img src={peanutTeton} className="pic-1"></img>
-                    <div className="pic-2"></div>
-                    <div className="pic-3"></div>
-                    <div className="pic-4"></div>
-                    <div className="pic-5"></div>
-                    <div className="pic-6"></div>
+                    <img className="pic-2"></img>
+                    <img className="pic-3"></img>
+                    <img className="pic-4"></img>
+                    <img className="pic-5"></img>
+                    <img className="pic-6"></img>
                 </div>
             </div>
         </div>
