6cf157f60d34c222097a8003b8313829b8e1aa60
SLeBron <sophieclebron@gmail.com>
2024-05-07 12:51:32 -0600

    added checkbox to list

--- src/pages/list/list.js
+++ src/pages/list/list.js
@@ -5,6 +5,14 @@ function List() {
         <div className="bucket-list">
             <h2 className="list-header">Bucket List</h2>
             <div className="list-item">Acadia National Park</div>
+            <label key="Acadia National Park" className="list-item">
+                <input 
+                    type="checkbox"
+                    className="selectbox"
+                    // checked={write code to add this park to array of parks that user has visited}
+                />
+                Acadia National Park
+            </label>
         </div>
     </div>
 }
