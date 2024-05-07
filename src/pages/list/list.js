91176c6e1c448aa329147aeaa783d6041daf53a5
SLeBron <sophieclebron@gmail.com>
2024-05-06 21:11:20 -0600

    updated list

--- src/pages/list/list.js
+++ src/pages/list/list.js
@@ -4,7 +4,7 @@ function List() {
     <div className="list-container">
         <div className="bucket-list">
             <h2 className="list-header">Bucket List</h2>
-            <div></div>
+            <div className="list-item">Acadia National Park</div>
         </div>
     </div>
 }
